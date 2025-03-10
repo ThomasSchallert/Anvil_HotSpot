d17fb63fcc736ae625e6f33afc9144783a0ecaad
BlJanosch <167287612+BlJanosch@users.noreply.github.com>
2025-03-10 14:30:44 +0100

    Delete test2.py

--- test2.py
+++
@@ -1,8 +0,0 @@
-import deviceController as dc 
-
-controller = dc.DeviceController('http://172.20.10.10:80') # IP-Adresse von wifi.py hier einfügen
-
-controller.down(10, 'cm')
-controller.up(10, 'cm')
-controller.right(10, 'cm')
-controller.left(10, 'cm')
